3ec74d0ba4c1797bc9eaf9e8f89a9eaff6c651b0
Akira Kawata <akirakawata1@gmail.com>
2020-05-06 16:03:47 +0900

    Call two times.

--- main.c
+++ main.c
@@ -3,9 +3,11 @@ int my_puts(char *str);
 void my_exit();
 
 void fun(){
-    char str[] = "Hello World!";
+    char str1[] = "Hello World!\n";
+    char str2[] = "=== simple linker ===\n";
 
-    my_puts(str);
+    my_puts(str1);
+    my_puts(str2);
 }
 
 int _start(){
